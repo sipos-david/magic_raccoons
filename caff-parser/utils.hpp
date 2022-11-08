66f41acc140799924266d3f0d41d0bab52be44e4
Zsófia Vozák <vozakzsofi@gmail.com>
2022-11-08 17:31:18 +0100

    utils

---
+++ caff-parser/utils.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using byte = unsigned char;
+
+int bytesToInt (std::vector<byte> bytes){
+    int number = 0;
+
+    for(std::size_t i = 0; i < bytes.size(); ++i){
+        number += bytes[i] * pow(16,i*2);
+    }
+    
+    return number;
+}
