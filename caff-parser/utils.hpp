4a72ee20fb1c78c9f9fd81fda5d02e927b458d74
Zsófia Vozák <vozakzsofi@gmail.com>
2022-11-08 17:41:19 +0100

    format docs

--- caff-parser/utils.hpp
+++ caff-parser/utils.hpp
@@ -1,13 +1,16 @@
 #include <vector>
+#include "math.h"
 
 using byte = unsigned char;
 
-int bytesToInt (std::vector<byte> bytes){
+int bytesToInt(std::vector<byte> bytes)
+{
     int number = 0;
 
-    for(std::size_t i = 0; i < bytes.size(); ++i){
-        number += bytes[i] * pow(16,i*2);
+    for (std::size_t i = 0; i < bytes.size(); ++i)
+    {
+        number += bytes[i] * std::pow(16, i * 2);
     }
-    
+
     return number;
 }
